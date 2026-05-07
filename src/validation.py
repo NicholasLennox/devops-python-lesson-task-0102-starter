06d0be4735f79d8ee2ea704915a882ca2a1d978d
Nicholas Lennox <30431725+NicholasLennox@users.noreply.github.com>
2026-05-07 10:58:38 +0200

    feat: added validate_percentage with tests

--- src/validation.py
+++ src/validation.py
@@ -1,5 +1,11 @@
 def validate_non_negative(value): 
-   if value < 0: 
+    if value < 0: 
        raise ValueError("Value cannot be negative") 
  
-   return True 
+    return True 
+ 
+def validate_percentage(value):
+    if(value < 0 or value > 100):
+        raise ValueError("Please enter a number between 0-100")
+    
+    return True
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,5 +1,5 @@
 import pytest 
-from validation import validate_non_negative 
+from validation import validate_non_negative, validate_percentage 
  
 def test_validate_non_negative_accepts_positive_numbers(): 
    result = validate_non_negative(10) 
@@ -10,4 +10,56 @@ def test_validate_non_negative_rejects_negative_numbers():
    with pytest.raises(ValueError): 
        validate_non_negative(-5)    
 
+def test_validate_percentage_valid_number():
+   # Arrange
+   value = 50
 
+   # Act
+   result = validate_percentage(value)
+
+   # Assert
+   assert result is True
+
+def test_validate_percentage_lower_bound():
+   # Arrange
+   value = 0
+
+   # Act
+   result = validate_percentage(value)
+
+   # Assert
+   assert result is True
+
+def test_validate_percentage_upper_bound():
+   # Arrange
+   value = 100
+
+   # Act
+   result = validate_percentage(value)
+
+   # Assert
+   assert result is True
+
+def test_validate_percentage_below_valid_should_throw():
+   # Arrange
+   value = -1
+   error_text = "Please enter a number between 0-100"
+
+   # Act + assert
+   with pytest.raises(ValueError) as error:
+      validate_percentage(value)
+
+   # Assert
+   assert str(error.value) == error_text
+
+def test_validate_percentage_above_valid_should_throw():
+   # Arrange
+   value = 101
+   error_text = "Please enter a number between 0-100"
+
+   # Act + assert
+   with pytest.raises(ValueError) as error:
+      validate_percentage(value)
+
+   # Assert
+   assert str(error.value) == error_text
